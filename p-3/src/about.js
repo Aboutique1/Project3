6396e64f32fa3d4e70c443125cc51834319a875d
AmberWebb <amberrw@yahoo.com>
2019-11-09 13:18:20 -0800

    Setting up Home and Contact page

---
+++ p-3/src/about.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import '/'
+
+function About() {
+  return (
+    <div className="App">
+     <h2>About</h2> 
+    </div>
+  );
+}
+
+export default About;
---
+++ p-3/src/contactUs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function ContactUs() {
+  return (
+    <div className="App">
+     <h2>Contact Us</h2> 
+    </div>
+  );
+}
+
+export default ContactUs;
